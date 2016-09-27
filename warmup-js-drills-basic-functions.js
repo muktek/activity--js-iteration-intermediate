3230c3f5f917833da93582dae184a141dfbaa597
Travis Hubbard <t3patterson@gmail.com>
2016-09-27 09:49:35 -0400

    js file commit

---
+++ warmup-js-drills-basic-functions.js
@@ -0,0 +1,32 @@
+//Warmup - JS Drills - Basic Functions & Data Types
+
+
+
+//------------------------------------------------------------------
+// TASK 1
+// Write a function called isEven() that tells whether a number is even or not.
+//------------------------------------------------------------------
+console.assert(isEven(1) === false)
+console.assert(isEven(2) === true)
+console.assert(isEven(22) === true)
+console.assert(isEven(33) === false)
+console.assert(isEven(56) === true)
+console.assert(isEven(57) === false)
+
+
+//------------------------------------------------------------------
+// TASK 2
+// Write a function called addEllipsis(). it should take a string
+// as input, and return a modified version of that string with an ellipsis ('...') on the end
+//
+// e.g. 'whatever man' becomes 'whatever man...'
+//
+
+console.assert(addEllipsis("Hello, Aunt Ellen") === "Hello, Aunt Ellen...")
+console.assert(addEllipsis("Welcome to my party") === "Welcome to my party...")
+
+
+// BONUS: It should also strip ending punctuation if there is any.
+//--------
+//console.assert(addEllipsis("I don't know her") === "I don't know her...")
+//console.assert(addEllipsis("It's the end of the world!") === "It's the end of the world...")
